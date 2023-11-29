ddb3823ae6ba650b71d859cafdf7e5531d371ed1
federico <pontonio.federico@gmail.com>
2023-11-29 09:18:17 +0100

    File names manipulated

--- javascript/rockPaperScissors/script.js
+++ javascript/rockPaperScissors/script.js
@@ -1,10 +1,12 @@
+
+
 //First goal:create randomly generated rock, paper or scissors.
 //declare array of choices
-let computerChoice = ["Rock", "Paper", "Scissors"];
+let possibleSelections = ["Rock", "Paper", "Scissors"];
 
 let getComputerChoice;
 //randomply pick an array index and assign it to getComputerChoice
-getComputerChoice = computerChoice[Math.floor(Math.random() * computerChoice.length)];
+getComputerChoice = possibleSelections[Math.floor(Math.random() * possibleSelections.length)];
 
 console.log(getComputerChoice);
 
@@ -12,12 +14,13 @@ console.log(getComputerChoice);
     //and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock".
 //Additional notes:
     //Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation),Account for TIES by re-playing the round.
-    
+
+
 //create random selector for players' default prompt value. this is necessary to be distinct for the computer function in order to 1-keep secret computer selection 2-avoid constant tie
-let defaultPromptValue = computerChoice[Math.floor(Math.random() * computerChoice.length)];
+let defaultPromptValue = possibleSelections[Math.floor(Math.random() * possibleSelections.length)];
 //declare playerSelection
 let playerInput = prompt("Enter your selection", defaultPromptValue)
 
 //declare computerSelection
 let computerSelection = getComputerChoice;
-//function that evaluates who wins
+//function that evaluates who wins a single round: a switch statement for each computer choice. inside, a case that evaluates the player's choice with the computer's. Lastly it returns the result.
