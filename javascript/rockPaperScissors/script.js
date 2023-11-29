504e1840580dc714f4305e5a3d9424ac39be207c
federico <pontonio.federico@gmail.com>
2023-11-29 10:45:34 +0100

    Add single match implementation
    Some things had to be moved from first step of the assignment to the second step, in order to include them in the scope of the function "matchEvaluation".
    
    Console logs to be removed eventually.
    
    Default prompt input from user is now correctely picking random values.

--- javascript/rockPaperScissors/script.js
+++ javascript/rockPaperScissors/script.js
@@ -1,14 +1,9 @@
 
 
-//First goal:create randomly generated rock, paper or scissors.
+//First goal:create randomly generated rock, paper or scissors. (moved to second goal)
 //declare array of choices
 let possibleSelections = ["Rock", "Paper", "Scissors"];
 
-let getComputerChoice;
-//randomply pick an array index and assign it to getComputerChoice
-getComputerChoice = possibleSelections[Math.floor(Math.random() * possibleSelections.length)];
-
-console.log(getComputerChoice);
 
 //Second goal:function that plays a single round of the game. The function should take two parameters - the playerSelection and computerSelection
     //and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock".
@@ -16,11 +11,71 @@ console.log(getComputerChoice);
     //Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation),Account for TIES by re-playing the round.
 
 
-//create random selector for players' default prompt value. this is necessary to be distinct for the computer function in order to 1-keep secret computer selection 2-avoid constant tie
-let defaultPromptValue = possibleSelections[Math.floor(Math.random() * possibleSelections.length)];
-//declare playerSelection
-let playerInput = prompt("Enter your selection", defaultPromptValue)
 
-//declare computerSelection
-let computerSelection = getComputerChoice;
-//function that evaluates who wins a single round: a switch statement for each computer choice. inside, a case that evaluates the player's choice with the computer's. Lastly it returns the result.
+let startButton = document.getElementById("startGame");
+startButton.addEventListener("click", matchEvaluation);
+
+//declare function matchEvaluation
+function matchEvaluation() {
+    //declare computer selection
+    let computerSelection = possibleSelections[Math.floor(Math.random() * possibleSelections.length)];
+
+    //create random selector for players' default prompt value.
+let defaultPromptValue = possibleSelections[Math.floor(Math.random() * possibleSelections.length)];
+    //declare player selection
+    let playerSelection = prompt("Enter your selection", defaultPromptValue);
+
+    //debug and control logs
+    console.log("opponent choice: " + computerSelection); //to be removed
+    console.log("your choice: " + playerSelection);       //to be removed
+    
+    //start conditional statements
+    //if computerSelection == "Rock"
+    if (computerSelection == "Rock") {
+
+        //if playerSelection == "Rock" THEN tie
+        if (playerSelection == "Rock") {
+            console.log("It's a tie! C'mon, try again!");
+        }
+        //if playerSelection == "paper" THEN win
+        if (playerSelection == "Paper") {
+            console.log("You won! Lucky you!");
+        }
+        //if playerSelection == "Scissors" THEN lose
+        if (playerSelection == "Scissors") {
+            console.log("You lost. Better luck next time :'(");
+        }
+    }    
+    //if computerSelection == "Paper"
+    if (computerSelection == "Paper") {
+
+        //if playerSelection == "Rock"
+        if (playerSelection =="Rock") {
+            console.log("You lost. Better luck next time :'(");
+        }
+        //if playerSelection == "paper"
+        if (playerSelection == "Paper") {
+            console.log("It's a tie! C'mon, try again!");
+        }
+        //if playerSelection == "Scissors"
+        if (playerSelection == "Scissors") {
+            console.log("You won! Lucky you!");
+        }
+    }    
+    //if computerSelection == "Scissors"
+    if (computerSelection == "Scissors"){
+
+        //if playerSelection == "Rock"
+        if (playerSelection == "Rock") {
+            console.log("You won! Lucky you!");
+        }
+        //if playerSelection == "paper"
+        if (playerSelection == "Paper") {
+            console.log("You lost. Better luck next time :'(");
+        }
+        //if playerSelection == "Scissors"
+        if (playerSelection == "Scissors") {
+            console.log("It's a tie! C'mon, try again!");
+        }
+    }
+}
